9f2bee7897aa1ce2c0a60de759e29fbf2cbecffc
muvuson <joelmuvuson7@gmail.com>
2025-03-18 12:42:37 +0200

    Commit #2
    In the NV from line 20 to 39, Book class is created to Represent a book with author and page count, extending GameObject.

--- src/GameObject.java
+++ src/GameObject.java
@@ -17,3 +17,23 @@ abstract class GameObject {
         return base * 1.15;
     }
 }
+class Book extends GameObject {
+    String author;
+    int pages;
+
+    public Book(String id, String name, LocalDate created, String author, int pages) {
+        super(id, name, created);
+        this.author = author;
+        this.pages = pages;
+    }
+    void displayDetails() {
+        System.out.println("Book: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("By " + author);
+        System.out.println(pages + " pages");
+        System.out.println("Added on " + created);
+    }
+    double calculatePrice(double base) {
+        return super.calculatePrice(base) + (pages * 0.03);
+    }
+}
